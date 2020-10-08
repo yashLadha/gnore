55f27380ad407f2daa1c74154da4c4328999f97a
Yash Ladha <yashladhapankajladha123@gmail.com>
2020-10-08 10:27:36 +0530

    chore: added flag for interactive and list
    Added flag for the passing the language list in a non-interactive mode.
    Thus saving the extra network call for fetching the templates from the
    API.

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 #[structopt(name = "ignore", about = "Add gitignore to your project in easy way")]
 struct Opt {
-    #[structopt(short, long = "ignore-list")]
-    ignore: bool,
+    #[structopt(short, long)]
+    interactive: bool,
+
+    #[structopt(short = "-l", long)]
+    ignore_list: Option<String>,
 }
 
 #[derive(Deserialize)]
@@ -58,7 +61,7 @@ async fn fetch_gitignore_request(ignore_list: String) -> Result<(), reqwest::Err
 
     match find_git_directory().await {
         Ok(directory) => {
-            println!("Git directory founded {}", directory);
+            println!("Git directory founded in {}", directory);
             create_gitignore(directory, ignore_text);
         }
 
@@ -70,7 +73,9 @@ async fn fetch_gitignore_request(ignore_list: String) -> Result<(), reqwest::Err
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
-    let interactive_list = opt.ignore;
+    let interactive_list = opt.interactive;
+
+    let mut ignore_list: String = String::from("");
 
     let template_string = find_templates().await?;
     let template_vec = parse_templates(&template_string);
@@ -78,9 +83,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     if interactive_list {
         prompt::prompt_begin();
         prompt::render_interactive_selector();
+    } else {
+        match opt.ignore_list {
+            Some(x) => {
+                ignore_list = x;
+            }
+            None => {
+                eprintln!("No value passed for the ignore list in non-inteactive mode");
+                process::exit(1);
+            }
+        }
     }
 
-    // fetch_gitignore_request(ignore_list).await?;
+    fetch_gitignore_request(ignore_list).await?;
 
     Ok(())
 }
